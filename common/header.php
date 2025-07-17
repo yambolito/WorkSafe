38cee2a07eb802e001bf8bfe2a04ea2d2ca21b99
Alexander Chagua <alexanderelton2887@gmail.como>
2025-07-16 22:24:16 -0500

    new

--- common/header.php
+++ common/header.php
@@ -8,12 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle : 'worksafe - sytem safe'; ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="/worksafe/worksafe/css/style.css">
+    <link rel="stylesheet" href="/worksafe/css/style.css">
 </head>
 <body>
     <header class="header">
         <div class="logo-container">
-            <img src="/worksafe/worksafe/images/logo.jpeg" alt="worksafe Logo" class="logo">
+            <img src="/worksafe/images/logo.jpeg" alt="worksafe Logo" class="logo">
             <div class="logo-text">Work<span>Safe</span></div>
         </div>
         
