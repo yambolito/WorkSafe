8d5b04db3d7beafa0e8455f7f1c3a777546e8476
Alexander Chagua <alexanderelton2887@gmail.como>
2025-07-16 22:26:41 -0500

    add file empty register

---
+++ views/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/worksafe/common/header.php"; ?>
+
+
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/worksafe/common/footer.php"; ?>
+</body>
+</html>
